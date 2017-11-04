8ef014535736002f46fa1082c9549ab7d438b221
Ronald Seoh <ronaldseoh@icloud.com>
2017-11-04 18:01:24 +0900

    branding change; directory cleanup

---
+++ app/src/main/java/net/iamblogger/wordlookup/Preferences.java
@@ -0,0 +1,18 @@
+package net.iamblogger.wordlookup;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Preferences extends PreferenceActivity {
+	/*TODO
+	 * Help page on how to use it
+	 * Quick search/lookup?
+	 */
+    //Called when the activity is first created. 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+    
+}
