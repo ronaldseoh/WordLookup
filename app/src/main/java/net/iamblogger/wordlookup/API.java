155b17d766d26cb22d0e8061e71cbe4188524b75
Ronald Seoh <ronaldseoh@icloud.com>
2017-11-07 20:47:55 +0900

    API.java: get the API key from res xml file instead of having it directly hard-coded in API.java

--- app/src/main/java/net/iamblogger/wordlookup/API.java
+++ app/src/main/java/net/iamblogger/wordlookup/API.java
@@ -26,63 +26,73 @@ public class API extends AsyncTask<String, Integer, String>{
 	}
 	
 	private static boolean Init() {
-	boolean networkState = isNetworkAvailable();
-	if (!networkState) return false;
+	     boolean networkState = isNetworkAvailable();
+
+	     if (!networkState) return false;
 	
-    // API key here
-    System.setProperty("WORDNIK_API_KEY", "1582dec71d5643a2532050252e10a8e7455e7887eafecc162");
-	return true;
+    	// API key here
+		// Instead of hard-coding the api key, I created another
+		// resource xml file to store my api key.
+		// If you want to compile this project yourself,
+		// go to http://developer.wordnik.com, get your own key,
+		// and store it as a string with name 'wordnik_api_key'
+		// (You probably would want to store that string in a separate xml file
+		// not tracked by git)
+		// NOTE: R.string.<name> does not return the string value itself;
+		// It just gives you the id
+    	System.setProperty("WORDNIK_API_KEY", "" + MainActivity.context.getString(R.string.wordnik_key));
+		return true;
 	}
 	
     public void GetDefinition(String[] wordarray) throws Exception {
 
     	//Lower case and clean up everything
-    word = wordarray[0];
-    String lcword = word.toLowerCase();
-    String lettersword = lcword.replaceAll("^[^\\p{L}\\p{N}]$",""); //removes symbols from first and last character
-    finalword = lettersword.replaceAll("[\\s]","%20"); //replaces spaces
-    	
-    if (!Init()) {
-    	
-    	result = word +": Error, check your network connection.";
-    	return;
-    }
-    
-	// check the status of the API key
-	TokenStatus status = AccountApi.apiTokenStatus();
-	if (status.isValid()) {
-	    Log.i("SWIFTDICT", "API key is valid.");
-	} else {
-	    Log.e("SWIFTDICT", "API key is invalid.");
-	    result = "API key is invalid!";
-	}
+		word = wordarray[0];
+		String lcword = word.toLowerCase();
+		String lettersword = lcword.replaceAll("^[^\\p{L}\\p{N}]$",""); //removes symbols from first and last character
+		finalword = lettersword.replaceAll("[\\s]","%20"); //replaces spaces
 
-	
-	// get a list of definitions for a word	
-	/* TODO:
-	 * It's a mess.
-	 * Make them optionable
-	 * CLEAN UP
-	 */
-	int limit = Integer.parseInt(MainActivity.NumOfDef);
-	boolean relatedwords = true;
-	boolean roots = true;
-	
-	List<Definition> def = WordApi.definitions(finalword,limit,null,relatedwords, null, roots, false);
+		if (!Init()) {
 
-	Log.i("SWIFTDICT","Found " + def.size() + " definitions.");
-	
-	for (Definition d : def) {
-		result = d.getWord() + ": \n";
-		word = d.getWord();
-	}
-	int i = 1;
-	for (Definition d : def) {
-		//		return((i++) + ") " + d.getPartOfSpeech() + ": " + d.getText());
-		result += i + ") " + d.getPartOfSpeech() + ": " + d.getText() + "\n";
-		Log.d("WORDLOOKUP",result);
-		i++;
-	}
+			result = word +": Error, check your network connection.";
+			return;
+		}
+
+		// check the status of the API key
+		TokenStatus status = AccountApi.apiTokenStatus();
+		if (status.isValid()) {
+			Log.i("SWIFTDICT", "API key is valid.");
+		} else {
+			Log.e("SWIFTDICT", "API key is invalid.");
+			result = "API key is invalid!";
+		}
+
+
+		// get a list of definitions for a word
+		/* TODO:
+		 * It's a mess.
+		 * Make them optionable
+		 * CLEAN UP
+		 */
+		int limit = Integer.parseInt(MainActivity.NumOfDef);
+		boolean relatedwords = true;
+		boolean roots = true;
+
+		List<Definition> def = WordApi.definitions(finalword,limit,null,relatedwords, null, roots, false);
+
+		Log.i("SWIFTDICT","Found " + def.size() + " definitions.");
+
+		for (Definition d : def) {
+			result = d.getWord() + ": \n";
+			word = d.getWord();
+		}
+		int i = 1;
+		for (Definition d : def) {
+			//		return((i++) + ") " + d.getPartOfSpeech() + ": " + d.getText());
+			result += i + ") " + d.getPartOfSpeech() + ": " + d.getText() + "\n";
+			Log.d("WORDLOOKUP",result);
+			i++;
+		}
 
     }
 
